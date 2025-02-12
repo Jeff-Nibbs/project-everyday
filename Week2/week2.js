899e0fad22fcd8c619df41a2f437342881068ec4
Jeffrey Nibbs <djcountry100@gmail.com>
2025-02-12 12:52:57 -0800

    add decorations

--- Week2/week2.js
+++ Week2/week2.js
@@ -3,8 +3,8 @@ function openCard(element) {
   if (activeCard == null) {
     element.classList.add("active");
   } else if (activeCard != element) {
-    activeCard.classList.remove("active");
     element.classList.add("active");
+    activeCard.classList.remove("active");
   } else if (activeCard == element) {
     element.classList.remove("active");
   }
